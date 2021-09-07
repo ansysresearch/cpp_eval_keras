7700549534407541ebe8405c963031c9436243e5
Haoliang Jiang <haoliang.jiang@ansys.com>
2021-09-06 23:15:20 -0400

    debugging

--- test.cc
+++ test.cc
@@ -138,7 +138,7 @@ int main() {
 		for (int j = 0; j < size; ++j)
 		{
 			test_data.at(i).at(j) = (test_data.at(i).at(j) - mean.at(j))/scale.at(j);
-			cout << out.data_[j] << " ";
+			cout << test_data.at(i).at(j) << " ";
 		}
 		cout << " " << "\n";
 
